d34e0fc9f4fa25c6460ce133971d4ebc74fb73b6
zumah_asy <60814220+prokomoke@users.noreply.github.com>
2020-05-08 08:27:03 +0700

    Add files via upload
    Untuk memenugi tugas di pekan ke-14

--- formula.java
+++ formula.java
@@ -1,98 +1,23 @@
 
 package week_12;
 
-import java.util.Scanner;
-
 class formula {
 
-    //1. Method
-    public void volume1 (){
-        Scanner input = new Scanner (System.in);
-
-    // 2.1 Variabel primitif
-        System.out.print("Luas Alas = ");
-        int luasAlas = input.nextInt ();
-        System.out.print("Tinggi = ");
-        int tinggi = input.nextInt ();
-        
-        System.out.println("volume = " + luasAlas * tinggi);
-    }
-    
-    public void volume2 (){
-        Scanner input = new Scanner (System.in);
-
-    // 2.1 Variabel primitif
-        System.out.print("Luas Alas = ");
-        int luasAlas = input.nextInt ();
-        System.out.print("Tinggi = ");
-        int tinggi = input.nextInt ();
-        
-        System.out.println("volume = " + ((luasAlas * tinggi)/3));
-    }
-    public void volume (){  
-        
+    // 1. Method
+    public void barisan (int a, int n ){
+  
     // 2.2 Variabel Array
-        String [] bangunRuang = new String [] {"Kubus", "Balok", "Prisma segitiga","Prisma segiempat", "tabung", "kerucut", "Limas segitiga", "Limas segiempat"};
-        
+       int [] perulangan = new int [n];
+
     // 3. Looping
-        for (int index1 = 0; index1 <= 5; index1++){
-            System.out.println("Volume "+ bangunRuang[index1]);
-            volume1();
-            System.out.print("\n");
-        }
-        
-        for (int index1 = 6; index1 < bangunRuang.length; index1++  ){
-            System.out.println("Volume "+ bangunRuang[index1]);
-            volume2();
+        for (int index = 0; index < perulangan.length; index++){
+            System.out.println("Banyak bakteri pada menit ke-" + index + " adalah = "+ (Math.pow(2, index)*a));
             System.out.print("\n");
         }
     }
     
-    //1. Method
-    public void luas1 (){
-        Scanner input = new Scanner (System.in);
-        
-    // 2.1 Variabel primitif
-        System.out.print("Alas/Panjang = ");
-        int base = input.nextInt ();
-        System.out.print("Tinggi/Lebar = ");
-        int height = input.nextInt ();
-        
-        System.out.println("Luas = " + base * height);
+    public void deret(int a, int n){
+       System.out.println("Jumlah bakteri setelah " + n + " menit adalah = " + a*(Math.pow(2, n)-1));
     }
-    
-    public void luas2 (){
-        Scanner input = new Scanner (System.in);
-       
-    // 2.1 Variabel primitif
-        System.out.print("Alas/Panjang = ");
-        int base = input.nextInt ();
-        System.out.print("Tinggi/Lebar = ");
-        int height = input.nextInt ();
-        
-        System.out.println("Luas = " + ((base * height)/2));
-    }
-    public void luas (){  
-    
-    // 2.2 Variabel Array
-        String [] bangunDatar = new String [] {"Persegi", "Persegi Panjang", "Jajar Genjang", "Segitiga"};
-        
-    // 3. Looping
-        for (int index2 = 0; index2 <= 3; index2++){
-            System.out.println("Luas "+ bangunDatar[index2]+ "\n");
-            luas1();
-            System.out.print("\n");
-        }
-        
-        for (int index2 = 4; index2 < bangunDatar.length; index2++  ){
-            System.out.println("Luas "+ bangunDatar[index2]+ "\n");
-            luas2();
-            System.out.print("\n");
-        }
-    }
+
 }
-
-
-   
-
-
--- hitung.java
+++ hitung.java
@@ -1,5 +1,5 @@
 
-package week_12;
+package pekan_14;
 
 import java.util.Scanner;
 
@@ -9,25 +9,39 @@ public class hitung {
         Scanner input = new Scanner (System.in);
         
         formula hitung = new formula ();
-
+        
+        System.out.print("PERKEBANGBIAKAN AMOEBA \n");
+        
+        System.out.print("Waktu (dalam menit) = ");
+    // 2.1 Variabel Primitif
+        int time = input.nextInt();
+        System.out.print("Banyak Amoeba mula-mula = ");
+        int sukuPertama = input.nextInt();
+        System.out.print("\n");       
+       
         System.out.print ("Manakah yang ingin anda hitung ? \n" );
-        System.out.print("1. Volume bangun ruang \n");
-        System.out.print("2. Luas bangun datar \n");
+        System.out.print("1. Banyak bakteri dalam tiap menitnya selama n menit \n");
+        System.out.print("2. Jumlah bakteri dalam n menit \n");
+        System.out.print("3. Hitung Semua \n");
         
         System.out.print ("Pilihan saya = ");
         int pilihan = input.nextInt();
         System.out.print("\n");
         
-        //4. Branching
+    //4. Branching
         switch (pilihan){
             case 1:
-                System.out.println("MENGHITUNG VOLUME BANGUN RUANG \n");
-                hitung.volume();
-            break;
+                System.out.print("PENAMBAHAN AMOEBA TIAP MENIT \n");
+                System.out.print("---------------------------- \n");              
+                hitung.barisan(sukuPertama,time);
+        break;
             case 2:
-                System.out.println("MENGHITUNG LUAS BANGUN DATAR \n");
-                hitung.luas();
-            break;
+                hitung.deret(sukuPertama,time);            
+        break;
+            case 3:
+                hitung.barisan(sukuPertama, time);
+                hitung.deret(sukuPertama, time);
+        break;
         }
     }
 
